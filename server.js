1e7089a4146f97601a106d236c659adf68821292
KevinFan <kevin_ypfan@yahoo.com.tw>
2017-10-18 09:23:53 +0800

    some fix

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.all('/*', function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,PATCH,OPTIONS');
   res.header('Access-Control-Allow-Credentials', true);
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Connection, User-Agent, Cookie, token')
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Connection, User-Agent, Cookie')
   res.header('Access-Control-Expose-Headers', 'token');
   next();
 });
